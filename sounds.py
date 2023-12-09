bac9cb00abd283812e41b3eb61f1953d39669a81
yongsik0710 <yongbin07100710@gmail.com>
2023-12-09 23:46:04 +0900

    Sound file fix

--- sounds.py
+++ sounds.py
@@ -2,10 +2,4 @@ import pygame
 from config import *
 
 
-def set_volume(volume):
-    for sound in sounds:
-        sound.set_volume(volume)
-
-
-explode_sound = pygame.mixer.Sound(Resources.Sounds.explode)
-shoot_sound = pygame.mixer.Sound(Resources.Sounds.shoot)
+pass
